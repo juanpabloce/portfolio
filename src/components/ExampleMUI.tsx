c11e9e4d72e01d2389b90ca1415a53629b301350
Juan Pablo Caguasango Enriquez <juan.enriquez@masiv.com>
2023-02-15 19:46:49 -0500

    Material UI and Icons is added

---
+++ src/components/ExampleMUI.tsx
@@ -0,0 +1,11 @@
+import Button from '@mui/material/Button';
+
+function ExampleMUI() {
+  return (
+    <div>
+      <Button variant="contained">Hello World</Button>
+    </div>
+  );
+}
+
+export { ExampleMUI };
